5b1617a4a489f5b65ca4a24283605b25252e37e8
Benjamin Roberts <Benjamin@BGRoberts.id.au>
2015-03-10 12:31:24 +1100

    install maven using puppet module

--- puppet/manifests/site.pp
+++ puppet/manifests/site.pp
@@ -1,24 +1,11 @@
-# Repos
-wget::fetch { "maven-repo":
-    source => "https://repos.fedorapeople.org/repos/dchen/apache-maven/epel-apache-maven.repo",
-    destination => "/etc/yum.repos.d/apache-maven.repo"
-} -> file { "/etc/yum.repos.d/apache-maven.repo":
-    ensure => "present",
-    owner => "root",
-    group => "root",
-    mode => 0644
-}
-
 # Required packages
 package { "openjdk-7":
     name => "java-1.7.0-openjdk-devel.x86_64",
     ensure => "present"
 }
 
-package { "apache-maven":
-    name => "apache-maven",
-    ensure => "present",
-    require => File["/etc/yum.repos.d/apache-maven.repo"]
+class { "maven::maven":
+
 }
 
 # Get maven packages
@@ -40,7 +27,7 @@ file { "/home/vagrant/.m2":
     command => "/usr/bin/mvn clean",
     cwd => "/open-daylight/controller",
     user => "vagrant",
-    require => [File["/home/vagrant/.m2/settings.xml"], Package["apache-maven"]],
+    require => [File["/home/vagrant/.m2/settings.xml"], Class["maven::maven"]],
     refreshonly => true,
     timeout => 0
 }
